e45f49fc0306b09cbcc6837291096c83c80cea51
DevilHunterFang <90432786+DevilHunterFang@users.noreply.github.com>
2023-04-07 23:20:07 +0900

    FX

--- src/main/java/shadowverse/Shadowverse.java
+++ src/main/java/shadowverse/Shadowverse.java
@@ -2525,6 +2525,8 @@ public class Shadowverse implements PostInitializeSubscriber, EditCardsSubscribe
         BaseMod.addCard( new MajesticSorcery());
         BaseMod.addCard( new AnneAndGrea());
         BaseMod.addCard( new Lyelth());
+        BaseMod.addCard( new LyelthMarionette());
+        BaseMod.addCard( new UnseenStrength());
         logger.info("Success");
     }
 

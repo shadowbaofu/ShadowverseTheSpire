f1b4fa2438571dc2037288ee5323e6a57d569f85
石涵澎 <1535193592@qq.com>
2023-06-22 12:28:53 +0800

    Update AbstractEnhanceCard.java

--- src/main/java/shadowverse/cards/AbstractEnhanceCard.java
+++ src/main/java/shadowverse/cards/AbstractEnhanceCard.java
@@ -14,12 +14,24 @@ public abstract class AbstractEnhanceCard extends CustomCard {
     public int baseCost;
     public int exEnhanceCost;
 
+    public boolean exFreeOnce;
+
+    public int exCost;
+
+    public int exCostForTurn;
+
+    public int ex;
+
     public AbstractEnhanceCard(String id, String name, String img, int cost, String rawDescription, CardType type, CardColor color, CardRarity rarity, CardTarget target,
                                int enhanceCost, int exEnhanceCost) {
         super(id, name, img, cost, rawDescription, type, color, rarity, target);
         this.baseCost = cost;
         this.enhanceCost = enhanceCost;
         this.exEnhanceCost = exEnhanceCost;
+        this.exCost = cost;
+        this.exCostForTurn = cost;
+        this.exFreeOnce = false;
+        this.ex = 0;
     }
 
     public AbstractEnhanceCard(String id, String name, String img, int cost, String rawDescription, CardType type, CardColor color, CardRarity rarity, CardTarget target,
@@ -33,11 +45,29 @@ public abstract class AbstractEnhanceCard extends CustomCard {
         super.update();
         if (AbstractDungeon.currMapNode != null && (AbstractDungeon.getCurrRoom()).phase == AbstractRoom.RoomPhase.COMBAT) {
             if (Shadowverse.Enhance(exEnhanceCost)) {
+                if (this.ex == 0) {
+                    this.exCost = this.cost;
+                    this.exCostForTurn = this.costForTurn;
+                }
+                this.ex = 2;
+                this.exFreeOnce = this.freeToPlayOnce;
                 setCostForTurn(exEnhanceCost);
             } else if (Shadowverse.Enhance(enhanceCost)) {
+                if (this.ex == 0) {
+                    this.exCost = this.cost;
+                    this.exCostForTurn = this.costForTurn;
+                }
+                this.ex = 1;
+                this.exFreeOnce = this.freeToPlayOnce;
                 setCostForTurn(enhanceCost);
             } else {
-                setCostForTurn(baseCost);
+                if (this.ex > 0){
+                    setCostForTurn(baseCost);
+                    this.cost = this.exCost;
+                    this.costForTurn = this.exCostForTurn;
+                    this.freeToPlayOnce = this.exFreeOnce;
+                }
+                this.ex = 0;
             }
         }
     }
@@ -45,17 +75,17 @@ public abstract class AbstractEnhanceCard extends CustomCard {
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
         if (enhanceCost != exEnhanceCost && this.costForTurn == exEnhanceCost && Shadowverse.Enhance(exEnhanceCost)) {
-            exEnhanceUse(p,m);
-            if (p.hasPower(CutthroatPower.POWER_ID)){
-                addToBot(new GainEnergyAction(exEnhanceCost-1));
+            exEnhanceUse(p, m);
+            if (p.hasPower(CutthroatPower.POWER_ID)) {
+                addToBot(new GainEnergyAction(exEnhanceCost - 1));
             }
         } else if (this.costForTurn == enhanceCost && Shadowverse.Enhance(enhanceCost)) {
-            enhanceUse(p,m);
-            if (p.hasPower(CutthroatPower.POWER_ID)){
-                addToBot(new GainEnergyAction(enhanceCost-1));
+            enhanceUse(p, m);
+            if (p.hasPower(CutthroatPower.POWER_ID)) {
+                addToBot(new GainEnergyAction(enhanceCost - 1));
             }
         } else {
-            baseUse(p,m);
+            baseUse(p, m);
         }
     }
 
